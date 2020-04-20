f76a5b073ad9987ce86515ceaa9ddc26a062d5ec
ædoardo <48515878+th3edo@users.noreply.github.com>
2020-04-20 20:04:45 +0200

    Primo commit dell'applicazione react

---
+++ front-end/src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import IndexComponent from "./components/indexComponent";
+
+ReactDOM.render(<IndexComponent />, document.getElementById("react-root"));
