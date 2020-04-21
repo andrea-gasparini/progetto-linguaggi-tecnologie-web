e91177e6dc6068eeb1a75a82e2a2bb44d43ccd59
ædoardo <48515878+th3edo@users.noreply.github.com>
2020-04-21 11:37:20 +0200

    formatting text

--- back-end/server/application/controllers/TestController.php
+++ back-end/server/application/controllers/TestController.php
@@ -17,14 +17,21 @@ class TestController extends REST_Controller {
 	public function index_get() {
 		/*
 		Il framework ha diverse configurazioni.
-		In config.php hai da settarti le varie cose tipo dove salvare le sessioni PHP, e alcune sulla sicurezza (tipo CSRF, e global_xss_filtering). Abbiamo info anche sui cookie e sulle sessioni appunto, ma comunque lato server questi non li useremo.
+		In config.php hai da settarti le varie cose tipo dove salvare le sessioni PHP, e alcune sulla sicurezza (tipo CSRF, 
+		e global_xss_filtering). Abbiamo info anche sui cookie e sulle sessioni appunto, ma comunque lato server questi non li useremo.
 		
 		Quindi le cose in /application/config/config.php è solo la base_url, non credo andremo ad utilizzare altro.
 		
-		Invece il file /application/config/autoload.php è importante. Qui praticamente possiamo caricare le librerie, packages, drivers, config e models senza doverli inizializzare in ogni controller.
-		Ad esempio se vedi c'è in Autoload il "TestModel" che si riferisce al file TestModel.php nella cartella application/models. In questa maniera possiamo accedere alle funzioni di quella classe senza dover inizializzarlo ogni volta in ogni controller (basta fare $this->TestModel->nomefunzione(parametri);).
+		Invece il file /application/config/autoload.php è importante. Qui praticamente possiamo caricare le librerie, packages, 
+		drivers, config e models senza doverli inizializzare in ogni controller.
+		Ad esempio se vedi c'è in Autoload il "TestModel" che si riferisce al file TestModel.php nella cartella application/models. 
+		In questa maniera possiamo accedere alle funzioni di quella classe senza dover inizializzarlo ogni volta in ogni controller (basta fare $this->TestModel->nomefunzione(parametri);).
 		
-		Sempre nell'autoload.php c'è un $autoload['helper'], praticamente diciamo che sono funzioni di "aiuto", come dice il nome. Ad esempio potremmo creare una funzione tipo "response_function($params) { ritorna un array con success e messaggio } (insomma, una funzione per le risposte API). Il file si chiamerà, se non ricordo male (magari vedi la docs), response_helper ad esempio, e quindi nell'autoload puoi fare $autoload['helper'] = array('response', etc);. Così in qualsiasi file (modello/controller/view che sia) potrai usare response_function();. Questo è comodo così se devi modificà la response basta che lo fai una sola volta. 
+		Sempre nell'autoload.php c'è un $autoload['helper'], praticamente diciamo che sono funzioni di "aiuto", come dice il nome. 
+		Ad esempio potremmo creare una funzione tipo "response_function($params) { ritorna un array con success e messaggio } (insomma, una funzione per le risposte API). 
+		Il file si chiamerà, se non ricordo male (magari vedi la docs), response_helper ad esempio, e quindi nell'autoload puoi fare $autoload['helper'] = array('response', etc);. 
+		Così in qualsiasi file (modello/controller/view che sia) potrai usare response_function();. 
+		Questo è comodo così se devi modificà la response basta che lo fai una sola volta. 
 		
 		Per quanto riguarda le funzioni che ti permettono di fare le query ti lascio la documentazione https://codeigniter.com/userguide3/database/queries.html non è niente di complicato.
 		Se hai domande ask me sempre.
