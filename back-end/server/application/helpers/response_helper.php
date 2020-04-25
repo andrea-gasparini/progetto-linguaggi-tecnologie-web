c927ec5b4bf623ed7919d8ae9da73c676568af4a
ædoardo <48515878+th3edo@users.noreply.github.com>
2020-04-25 18:04:09 +0200

    Update responseHelper
    Crea i dati da inviare al redux

--- back-end/server/application/helpers/response_helper.php
+++ back-end/server/application/helpers/response_helper.php
@@ -3,3 +3,9 @@
 function buildServerResponse($status, $message, $data = array()) {
 	return array("status" => $status, "message" => $message, "data" => $data);
 }
+
+function setUserDataToSendInRedux($user) {
+	return array(
+		"userId" => $user->id
+	);
+}
