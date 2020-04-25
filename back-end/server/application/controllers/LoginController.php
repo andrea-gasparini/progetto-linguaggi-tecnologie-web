b8ebb8db27de616cc35de6428a5b592432beb4d6
ædoardo <48515878+th3edo@users.noreply.github.com>
2020-04-25 09:21:45 +0200

    Aggiunto check della password nel PHP
    https://www.php.net/manual/en/function.password-hash.php (doc)

--- back-end/server/application/controllers/LoginController.php
+++ back-end/server/application/controllers/LoginController.php
@@ -13,11 +13,11 @@ class LoginController extends \chriskacerguis\RestServer\RestController
 
 		$username = $this->input->post('username');
 		$password = $this->input->post('password');
-		if(!$username && !$password || (strlen($username) <= 0 || strlen(trim($username)) <= 0) && strlen($password) <= 0) // controllo se username && password sono vuoti o non inviati.
+		if(!$username && !$password || (strlen($username) <= 0 || strlen(trim($username)) <= 0) && strlen($password) <= 0) // username && password sono vuoti o non inviati.
 			return $this->response(buildServerResponse(false, "Compila tutti i campi per poter eseguire l'accesso.",
 				array("usernameHasError" => true, "passwordHasError" => true)), 200);
 
-		if((!$username || strlen($username) <= 0 || strlen(trim($username)) <= 0) && strlen($password) > 0) // controllo se l'username NON è completo mentre la password sì
+		if((!$username || strlen($username) <= 0 || strlen(trim($username)) <= 0) && strlen($password) > 0) // l'username NON è completo mentre la password sì
 			return $this->response(buildServerResponse(false, "Compila il campo username per poter eseguire l'accesso.",
 				array("usernameHasError" => true, "passwordHasError" => false)), 200);
 
@@ -26,16 +26,22 @@ class LoginController extends \chriskacerguis\RestServer\RestController
 				array("usernameHasError" => false, "passwordHasError" => true)), 200);
 
 
-		// controlliamo che la mail sia valida lato server.
+		// check che l'email sia valida.
 		if(!FILTER_VAR($username, FILTER_VALIDATE_EMAIL))
-			return $this->response(buildServerResponse(false, "L'email da te inserita non è valida.",
+			return $this->response(buildServerResponse(false, "L'email inserita non è valida.",
 				array("usernameHasError" => true, "passwordHasError" => false)), 200);
 
-		// dobbiamo controllare se l'email (cioè username) è registrata nel nostro database.
+		// check se l'utente esiste nel database.
 		$user = $this->UserModel->getUserByEmail($username);
 		if(count($user) <= 0) // non esiste l'utente.
 			return $this->response(buildServerResponse(false, "Nessun utente registrato con questo indirizzo email.",
 				array("usernameHasError" => true, "passwordHasError" => false)), 200);
+
+		// check della password.
+		$userPassword = $user[0]->password;
+		if(!password_verify($password, $userPassword)) // la password inserita non è verificata
+			return $this->response(buildServerResponse(false, "I dati inseriti non sono corretti.",
+				array("usernameHasError" => true, "passwordHasError" => true)), 200);
 
 
 		return $this->response(array("status" => "success", "input" => $this->input->post()), 200);
