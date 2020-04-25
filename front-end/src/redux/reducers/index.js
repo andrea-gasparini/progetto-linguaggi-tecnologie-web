6cf17db23adb8b4bac86958dda38a37305c14f63
GaspRulez <gasptube@gmail.com>
2020-04-25 20:05:14 +0200

    Setup signUpReducer

--- front-end/src/redux/reducers/index.js
+++ front-end/src/redux/reducers/index.js
@@ -1,6 +1,8 @@
 import {combineReducers} from "redux";
 import {loginReducer} from "./login/loginReducer";
+import {signUpReducer} from "./signup/signUpReducer";
 
 export default combineReducers({
     loginReducer,
+    signUpReducer,
 });
---
+++ front-end/src/redux/reducers/signup/signUpReducer.js
@@ -0,0 +1,8 @@
+
+export const signUpReducer =
+    (state = { signUpRealname: '', signUpUsername: '', signUpPassword: ''}, action) => {
+        switch(action.type) {
+            default:
+                return state;
+        }
+    };
