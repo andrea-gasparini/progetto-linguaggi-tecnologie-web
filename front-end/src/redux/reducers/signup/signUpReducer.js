02a0cca8d65a6c89eca690264b25468d1cab8b0c
GaspRulez <gasptube@gmail.com>
2020-04-25 20:34:07 +0200

    Definizione actions signup reducer

--- front-end/src/redux/reducers/signup/signUpReducer.js
+++ front-end/src/redux/reducers/signup/signUpReducer.js
@@ -1,8 +1,31 @@
+import update from "immutability-helper";
+import {
+    SET_CONFIRM_PASSWORD_SIGNUP,
+    SET_EMAIL_SIGNUP,
+    SET_PASSWORD_SIGNUP,
+    SET_REALNAME_SIGNUP,
+    SET_USERNAME_SIGNUP
+} from "../../actions/signup/action";
 
 export const signUpReducer =
-    (state = { signUpRealname: '', signUpUsername: '', signUpPassword: ''}, action) => {
+    (state = { signUpRealname: '', signUpUsername: '', signUpEmail: '', signUpPassword: '', signUpConfirmPassword: ''}, action) => {
         switch(action.type) {
             default:
                 return state;
+
+            case SET_REALNAME_SIGNUP:
+                return;
+
+            case SET_USERNAME_SIGNUP:
+                return;
+
+            case SET_EMAIL_SIGNUP:
+                return;
+
+            case SET_PASSWORD_SIGNUP:
+                return;
+
+            case SET_CONFIRM_PASSWORD_SIGNUP:
+                return;
         }
     };
