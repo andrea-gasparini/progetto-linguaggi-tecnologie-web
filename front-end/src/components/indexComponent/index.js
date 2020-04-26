6584c6c4505994eaac1ae221d29381b7f2b5480f
ædoardo <48515878+th3edo@users.noreply.github.com>
2020-04-26 20:06:00 +0200

    fix

--- front-end/src/components/indexComponent/index.js
+++ front-end/src/components/indexComponent/index.js
@@ -100,7 +100,7 @@ class IndexComponent extends Component {
 
                                         <div>
                                             <button disabled={username.length <= 0 || password.length <= 0}
-                                                    onClick={(e) => this.signIn(e)}
+                                                    onSubmit={(e) => this.signIn(e)}
                                                     className={"btn btn-primary sapienzaButton"}>Accedi
                                             </button>
                                         </div>
