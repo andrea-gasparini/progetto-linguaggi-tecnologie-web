ea90abb07c1eab53bbe43be5549eed47ceca2577
ædoardo <48515878+th3edo@users.noreply.github.com>
2020-04-27 12:02:22 +0200

    Stile home
    Cominciato lo stile della Home.

---
+++ front-end/src/components/groupCardComponent/groupCardComponent.js
@@ -0,0 +1,19 @@
+import React, {Component, Fragment} from "react";
+
+class GroupCardComponent extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return(
+            <Fragment>
+                <div className={"groupCard"}>
+                    
+                </div>
+            </Fragment>
+        )
+    }
+}
+
+export default GroupCardComponent;
--- front-end/src/components/homeComponent/index.js
+++ front-end/src/components/homeComponent/index.js
@@ -1,5 +1,6 @@
 import React, {Component, Fragment} from "react";
 import HeaderComponent from "../headerComponent";
+import './style.css';
 
 class HomeComponent extends Component {
     constructor(props) {
@@ -10,6 +11,17 @@ class HomeComponent extends Component {
         return(
             <Fragment>
                 <HeaderComponent />
+                <section className={"d-flex justify-content-center"}>
+                    <div className={"d-flex homeContainerGroups flex-column"}>
+                        <div className={"myGroupsTitle text-muted"}>
+                            I miei gruppi (5)
+                        </div>
+
+                        <div className={"d-flex flex-row flex-wrap groupsList"}>
+
+                        </div>
+                    </div>
+                </section>
             </Fragment>
         )
     }
