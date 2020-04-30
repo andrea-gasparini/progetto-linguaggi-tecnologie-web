636ede3a2c88f168d449618b742c4d982cc2d7cf
ædoardo <48515878+th3edo@users.noreply.github.com>
2020-04-30 09:17:47 +0200

    Funzione isGroupMember
    Controlla che l'utente sia un membro del gruppo.

--- back-end/server/application/models/GroupsModel.php
+++ back-end/server/application/models/GroupsModel.php
@@ -9,4 +9,12 @@ class GroupsModel extends CI_Model {
 		return $query->result();
 	}
 
+	public function isGroupMember($userId, $groupId) {
+		$this->db->select("*");
+		$this->db->where(array("user_id" => $userId, "group_id" => $groupId));
+		$this->db->limit(1);
+		$query = $this->db->get("groupsMemberships");
+		return $query->num_rows() > 0;
+	}
+
 }
