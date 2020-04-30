c53d0d37e303a5352d4c5faddb0a649501f902e4
ædoardo <48515878+th3edo@users.noreply.github.com>
2020-04-30 09:23:55 +0200

    Code refactoring
    Migliorato il codice: creata funzione unica per il check del token da chiamare nei controller che ne hanno bisogno.

--- back-end/server/application/controllers/SearchUserController.php
+++ back-end/server/application/controllers/SearchUserController.php
@@ -12,21 +12,9 @@ class SearchUserController extends \chriskacerguis\RestServer\RestController
 	public function searchUser_post()
 	{
 		// l'utente dev'essere loggato per cercare.
-		$token = $this->input->get_request_header('Authorization'); // Authorization header dalla richiesta post
-		if(strlen($token) >= 6 && substr($token, 0, 6) == "Bearer") {
-			$token = count(explode("Bearer ", $token)) > 0 ? explode("Bearer ", $token)[1] : ""; // il token ora è qualcosa della forma jwt oppure è vuoto.
-
-			if (strlen($token) <= 0) // token non valido in questo caso
-				return $this->response(buildServerResponse(false, "Token di accesso non valido. #0"), 200);
-
-			$tokenData = AUTHORIZATION::validateToken($token); // questa funzione chiama la decode di jwt che ritorna false nel caso in cui il token non sia valido, altrimenti ritorna il payload del token.
-			if (!$tokenData) // se il token non è stato decodificato correttamente
-				return $this->response(buildServerResponse(false, "Token di accesso non valido. #1"), 200);
-
-			$userId = $tokenData->userId;
-			if(!FILTER_VAR($userId, FILTER_VALIDATE_INT)) // se l'userId nel token non è un intero c'è un problema.
-				return $this->response(buildServerResponse(false, "Token di accesso non valido. #3"), 200);
-
+		$tokenData = validateAuthorizationToken($this->input->get_request_header('Authorization'));
+		if($tokenData["status"]) {
+			$userId = $tokenData["data"]["userId"];
 			$user = $this->UserModel->getUserById($userId); // i dati dell'utente.
 			if(count($user) <= 0) // utente non trovato con questo id
 				return $this->response(buildServerResponse(false, "Token di accesso non valido. #5"), 200);
@@ -35,6 +23,7 @@ class SearchUserController extends \chriskacerguis\RestServer\RestController
 			$result = $this->UserModel->searchUser($searchQuery, $userId);
 			return $this->response(buildServerResponse(true, "ok", array("searchResult" => $result)));
 		}
+		return $this->response(buildServerResponse(false, "Errore autorizzazione."), 200);
 	}
 
 }
--- back-end/server/application/helpers/response_helper.php
+++ back-end/server/application/helpers/response_helper.php
@@ -9,3 +9,25 @@ function setUserDataToSendInRedux($user) {
 		"userId" => $user->id
 	);
 }
+
+function validateAuthorizationToken($token) {
+	$token = $token; // Authorization header dalla richiesta post
+	if(strlen($token) >= 6 && substr($token, 0, 6) == "Bearer") {
+		$token = count(explode("Bearer ", $token)) > 0 ? explode("Bearer ", $token)[1] : ""; // il token ora è qualcosa della forma jwt oppure è vuoto.
+
+		if (strlen($token) <= 0) // token non valido in questo caso
+			return buildServerResponse(false, "Token di accesso non valido. #0");
+
+		$tokenData = AUTHORIZATION::validateToken($token); // questa funzione chiama la decode di jwt che ritorna false nel caso in cui il token non sia valido, altrimenti ritorna il payload del token.
+		if (!$tokenData) // se il token non è stato decodificato correttamente
+			return buildServerResponse(false, "Token di accesso non valido. #1");
+
+		$userId = $tokenData->userId;
+		if (!FILTER_VAR($userId, FILTER_VALIDATE_INT)) // se l'userId nel token non è un intero c'è un problema.
+			return buildServerResponse(false, "Token di accesso non valido. #2");
+
+		return buildServerResponse(true, "ok", array("userId" => $tokenData->userId));
+	}
+
+	return buildServerResponse(false, "Token di accesso non valido. #3");;
+}
