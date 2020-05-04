365e1d9e434a14204695c09430b1b36b149e9d5d
ædoardo <48515878+th3edo@users.noreply.github.com>
2020-05-04 21:01:47 +0200

    Started delete group modal

---
+++ front-end/src/components/modalsComponents/deleteGroupModalComponent/index.js
@@ -0,0 +1,10 @@
+import React, {Component} from "react";
+import {withCookies} from "react-cookie";
+
+class DeleteGroupModalComponent extends Component {
+    constructor(props) {
+        super(props);
+    }
+}
+
+export default withCookies(DeleteGroupModalComponent);
