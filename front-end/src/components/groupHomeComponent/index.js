e1cbcbc1f3578cea4ae6ae734a74705a4bd1b96d
GaspRulez <gasptube@gmail.com>
2020-05-08 16:47:14 +0200

    Scheletro home gruppo

--- front-end/src/components/groupHomeComponent/index.js
+++ front-end/src/components/groupHomeComponent/index.js
@@ -3,6 +3,8 @@ import HeaderComponent from "../headerComponent";
 import {validateToken} from "../../redux/actions/login";
 import {withCookies} from "react-cookie";
 import {connect} from "react-redux";
+import './style.css';
+import {Send, Users, Clipboard} from "react-feather";
 
 
 class GroupHomeComponent extends Component {
@@ -20,9 +22,35 @@ class GroupHomeComponent extends Component {
         let {history} = this.props;
 
         return (
-          <Fragment>
-              <HeaderComponent history={history} />
-          </Fragment>
+            <Fragment>
+                <HeaderComponent history={history} />
+                <section className={"d-flex justify-content-center"}>
+                    <div className={"home d-flex flex-column"}>
+                        <div className={"mb-3"}>
+                            <h1>Componente per la creazione di postaaaaaaaaaaa</h1>
+                        </div>
+                        <div className={"d-flex flex-row"}>
+                            <div className={"main-content mr-2 d-flex flex-column align-items-center"}>
+                                <h1>Post</h1>
+                            </div>
+                            <div className={"navigation-menu noselectText ml-2 d-flex flex-column"}>
+                                <div className={"menu-item"}>
+                                    <Send />
+                                    <span>Chat</span>
+                                </div>
+                                <div className={"menu-item"}>
+                                    <Clipboard />
+                                    <span>Bacheca</span>
+                                </div>
+                                <div className={"menu-item"}>
+                                    <Users />
+                                    <span>Membri</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </Fragment>
         );
     }
 
