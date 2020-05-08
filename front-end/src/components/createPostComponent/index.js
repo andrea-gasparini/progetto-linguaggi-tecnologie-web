45b4fab7b1cd6ce7f983233cd1650d7fa476e8fb
ædoardo <48515878+th3edo@users.noreply.github.com>
2020-05-08 09:22:58 +0200

    Started create post component.

---
+++ front-end/src/components/createPostComponent/index.js
@@ -0,0 +1,20 @@
+import React, {Component, Fragment} from "react";
+
+class CreatePostComponent extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <Fragment>
+                <div>
+                    
+                </div>
+            </Fragment>
+        );
+    }
+
+}
+
+export default CreatePostComponent;
