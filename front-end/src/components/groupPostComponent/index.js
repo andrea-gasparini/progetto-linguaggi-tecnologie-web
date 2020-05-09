e87a44e46558355daf743f696956e04cdad0cb91
GaspRulez <gasptube@gmail.com>
2020-05-09 22:33:05 +0200

    Setup GroupPost component

---
+++ front-end/src/components/groupPostComponent/index.js
@@ -0,0 +1,20 @@
+import React, {Component, Fragment} from "react";
+import './style.css';
+
+class GroupPostComponent extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+          <Fragment>
+            <h1>I'm a post</h1>
+          </Fragment>
+        );
+    }
+
+}
+
+export default GroupPostComponent;
