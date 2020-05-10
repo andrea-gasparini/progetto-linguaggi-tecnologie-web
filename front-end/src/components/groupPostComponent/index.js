b8ba1e57a73ee0820cf3ec747039637b2f237029
GaspRulez <gasptube@gmail.com>
2020-05-10 12:06:00 +0200

    Bozza stile create new comment

--- front-end/src/components/groupPostComponent/index.js
+++ front-end/src/components/groupPostComponent/index.js
@@ -1,27 +1,54 @@
 import React, {Component, Fragment} from "react";
+import {PlusCircle} from "react-feather";
 import './style.css';
 
 class GroupPostComponent extends Component {
 
     constructor(props) {
         super(props);
+
+        this.state = {
+            textareaMinHeight: undefined
+        }
     }
 
+    handleTextAreaValue = (e) => {
+        // setto sempre ad una riga l'altezza della textarea
+        if (! this.state.textareaMinHeight)
+            this.state.textareaMinHeight = e.target.scrollHeight;
+        else
+            e.target.style.height = this.state.textareaMinHeight + "px";
+
+        // in base a quanto scroll ci sarebbe con una sola riga, reimposto l'altezza
+        e.target.style.height = e.target.scrollHeight + "px";
+
+        //this.setState({postText: e.target.value});
+    };
+
     render() {
+
+        let {username, realname, publishDate, text} = this.props;
+
         return (
             <Fragment>
                 <div className={"post"}>
                     <div className={"post-header"}>
-                        <h1>Post title</h1>
+                        <img className={"profile-pic noselectText"} src={"https://pluspng.com/img-png/user-png-icon-male-user-icon-512.png"}/>
+                        <div className={"d-flex flex-column"}>
+                            <span className={"user-name"}>
+                                {username} {"(@" + realname + ")"}
+                            </span>
+                            <span className={"publish-date"}>
+                                {publishDate}
+                            </span>
+                        </div>
                     </div>
                     <div className={"post-body"}>
-                        <p>
-                            Care Studentesse, cari Studenti,
-                            Per accedere ai webinar delle esercitazioni, potete farlo via Classroom o direttamente tramite questo link:
-                            Google Meet: https://meet.google.com/fffffffffff
-                            Saluti,
-                            -- Andrea Gasparini
-                        </p>
+                        <p>{text}</p>
+                    </div>
+                    <div className={"post-new-comment"}>
+                        <textarea rows={1} placeholder={"Aggiungi un commento al post.."} onChange={e => this.handleTextAreaValue(e)}/>
+                        <PlusCircle className={"new-comment-icon"} />
                     </div>
                 </div>
             </Fragment>
