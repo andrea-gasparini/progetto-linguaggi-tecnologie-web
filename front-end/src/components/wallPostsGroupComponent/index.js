2c48d6235a7d4d100d4798dceff41cac5bff0260
ædoardo <48515878+th3edo@users.noreply.github.com>
2020-05-10 13:11:53 +0200

    Push bacheca e primo switch tra componenti

---
+++ front-end/src/components/wallPostsGroupComponent/index.js
@@ -0,0 +1,18 @@
+import React, {Component, Fragment} from "react";
+import GroupPostComponent from "../groupPostComponent";
+
+class WallPostsGroupComponent extends Component {
+
+    render() {
+        let {posts} = this.props;
+        return(
+            <Fragment>
+                {typeof posts !== "undefined" && posts.map((post, index) => (
+                    <GroupPostComponent realname={post.realname} username={post.username} publishDate={post.created_at} text={post.post_text} />
+                ))}
+            </Fragment>
+        )
+    }
+}
+
+export default WallPostsGroupComponent;
