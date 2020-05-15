edf273e6b72f24dd3288368cf2eeb1537a6f422b
ædoardo <48515878+th3edo@users.noreply.github.com>
2020-05-15 10:18:30 +0200

    Piccoli fix & start box chat
    Iniziato il box della chat e inserito qualche piccolo fix.

--- front-end/src/components/groupChatComponent/index.js
+++ front-end/src/components/groupChatComponent/index.js
@@ -1,6 +1,6 @@
 import React, {Component, Fragment} from "react";
 import './style.css';
-import {ChevronLeft, ChevronRight} from "react-feather";
+import {ChevronLeft, ChevronRight, Send} from "react-feather";
 
 class GroupChatComponent extends Component {
 
@@ -41,14 +41,14 @@ class GroupChatComponent extends Component {
         return(
             <Fragment>
                 <div className={"d-flex flex-column"} style={{width: "100%", position: "relative"}}>
-                    <div ref={(ref) => {this.scrollElement = ref}} className={"d-flex flex-row onlineUsers align-items-center p-3"}>
+                    <div ref={(ref) => {this.scrollElement = ref}} className={"d-flex flex-row onlineUsers align-items-center p-1"}>
                         {showScrollLeftUserOnline &&
                         <div onClick={() => this.scrollUsersOnline('left', 20, 700)}
                              className={"d-flex align-items-center justify-content-center arrowLeft"}><ChevronLeft
                             size={20}/></div>
                         }
                         {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20].map((user, index) => (
-                            <div key={index} className={"d-flex flex-column userBoxOnline justify-content-center align-items-center"}>
+                            <div title={`${user} è online ora!` } key={index} className={"d-flex flex-column userBoxOnline justify-content-center align-items-center"}>
                                 <div className={"userOnline"} />
                                 <div className={"text-muted"}>
                                     {user}
@@ -61,6 +61,20 @@ class GroupChatComponent extends Component {
                             size={20}/></div>
                         }
                         </div>
+                    <div className={"d-flex chatBox flex-column justify-content-between"}>
+                        <div className={"d-flex chatMessages flex-column"}>
+                            <div className={"d-flex message"}>
+                                <div className={"userIconMessage"}/>
+                                <div className={"messageText"}>
+                                    Ciao io sono edoaardo
+                                </div>
+                            </div>
+                        </div>
+                        <div className={"d-flex sendMessageInput align-items-center justify-content-between"}>
+                            <input type={"text"} className={"form-control messageInputArea"} placeholder={"Inserisci un messaggio..."} />
+                            <Send className={"sendMessageIcon"} />
+                        </div>
+                    </div>
                 </div>
             </Fragment>
         )
--- front-end/src/components/groupHomeComponent/index.js
+++ front-end/src/components/groupHomeComponent/index.js
@@ -18,7 +18,7 @@ class GroupHomeComponent extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { showActiveMenuItem: true, isLoadingPost: false, activeIndex: 1 };
+        this.state = { showActiveMenuItem: true, isLoadingPost: false, activeIndex: 0 };
 
         this.navigationItems = [
             {
