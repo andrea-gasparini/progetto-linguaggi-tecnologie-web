c4827b94721d4ef6a49bee679013fc67ab4d9e3d
GaspRulez <gasptube@gmail.com>
2020-05-16 13:12:10 +0200

    AddNewComment reduce function

--- front-end/src/redux/reducers/group/groupReducer.js
+++ front-end/src/redux/reducers/group/groupReducer.js
@@ -1,4 +1,4 @@
-import {ADD_GROUP_POSTS, ADD_NEW_POST, RESET_GROUP_DATA} from "../../actions/group/actions";
+import {ADD_GROUP_POSTS, ADD_NEW_COMMENT, ADD_NEW_POST, RESET_GROUP_DATA} from "../../actions/group/actions";
 import update from "immutability-helper";
 
 const groupReducer = (state = {groupPosts: [], hasOtherPostsToLoad: true, currentOffset: 0}, action) => {
@@ -25,6 +25,13 @@ const groupReducer = (state = {groupPosts: [], hasOtherPostsToLoad: true, curren
             return update(state, {
                 groupPosts: {$unshift: [action.payload.post]}
             });
+
+        case ADD_NEW_COMMENT:
+            return update(state, {
+                groupPosts: { [action.payload.postIndex]: {
+                        comments: { $push: [action.payload.newCommentValue] }
+                }}
+            })
     }
 };
 
