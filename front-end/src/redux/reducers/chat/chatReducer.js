837a119fcbb2fa391973c78e1dfbad48e7db60e1
ædoardo <48515878+th3edo@users.noreply.github.com>
2020-05-16 18:03:50 +0200

    Chat funzioni vitali finite, e fix bacheca

--- front-end/src/redux/reducers/chat/chatReducer.js
+++ front-end/src/redux/reducers/chat/chatReducer.js
@@ -1,7 +1,12 @@
-import {ADD_MESSAGE_TO_CHAT, ADD_MESSAGES_LOADED, RESET_CHAT_DATA} from "../../actions/chat/actions";
+import {
+    ADD_MESSAGE_TO_CHAT,
+    ADD_MESSAGES_LOADED,
+    RESET_CHAT_DATA,
+    SET_REQUESTING_CHAT_MESSAGES
+} from "../../actions/chat/actions";
 import update from "immutability-helper";
 
-const chatReducer = (state = {messages: [], offsetChatMessages: 0}, action) => {
+const chatReducer = (state = {messages: [], offsetChatMessages: 0, canLoadOtherMessagesChat: true, isRequestingChatMessages: false}, action) => {
     switch(action.type) {
         default:
             return state;
@@ -11,16 +16,25 @@ const chatReducer = (state = {messages: [], offsetChatMessages: 0}, action) => {
                 messages: {$push: [action.payload.message]}
             });
 
+        case SET_REQUESTING_CHAT_MESSAGES:
+            return update(state, {
+                isRequestingChatMessages: {$set: action.payload.value}
+            });
+
         case RESET_CHAT_DATA:
             return update(state, {
                 messages: {$set: []},
-                offsetChatMessages: {$set: 0}
+                offsetChatMessages: {$set: 0},
+                canLoadOtherMessagesChat: {$set: true},
+                isRequestingChatMessages: {$set: false}
             });
 
         case ADD_MESSAGES_LOADED: {
             return update(state, {
-                messages: {$unshift: action.payload.messages}
-            })
+                messages: {$unshift: action.payload.messages},
+                offsetChatMessages: {$set: action.payload.newOffset},
+                canLoadOtherMessagesChat: {$set: action.payload.canLoadOtherMessagesChat},
+            });
         }
     }
 };
