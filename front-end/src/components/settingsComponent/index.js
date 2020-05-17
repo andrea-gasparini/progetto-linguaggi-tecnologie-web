7e769107100c20c906a2273cb516aab68ecfd32c
GaspRulez <gasptube@gmail.com>
2020-05-17 19:26:06 +0200

    Inizio scheletro pagina settings

--- front-end/src/components/settingsComponent/index.js
+++ front-end/src/components/settingsComponent/index.js
@@ -1,6 +1,12 @@
-import 'style.css';
+import './style.css';
 import React, {Component, Fragment} from "react";
 import HeaderComponent from "../headerComponent";
+import {connect} from "react-redux";
+import {withCookies} from "react-cookie";
+import {validateToken} from "../../redux/actions/login";
+import {API_SERVER_URL} from "../../globalConstants";
+
+const mapStateToProps = (state) => ({...state.userReducer});
 
 class SettingsComponent extends Component {
 
@@ -8,15 +14,29 @@ class SettingsComponent extends Component {
         super(props);
     }
 
+    componentDidMount() {
+        let {dispatch, cookies, history} = this.props;
+        dispatch(validateToken(cookies, history, false, '/settings'));
+    }
+
     render() {
-        return (
-            <Fragment>
-                <HeaderComponent history={history} />
-                <p>Hello from settings component!</p>
-            </Fragment>
-        );
+        let {history, userData} = this.props;
+
+        return (typeof userData !== "undefined") ?
+            (
+                <Fragment>
+                    <HeaderComponent history={history} />
+                    <section className={"d-flex flex-column align-items-center justify-content-center"}>
+                        <div className={"d-flex flex-column align-items-center settingsContainer"}>
+                            <div className={"profilePic"} style={{backgroundImage: `url("${API_SERVER_URL}/uploads/profilePictures/${userData.viewer.picture}")`}}  />
+                            <p>Hello from settings component!</p>
+                        </div>
+                    </section>
+                </Fragment>
+            )
+        : <Fragment />
     }
 
 }
 
-export default SettingsComponent;
+export default withCookies(connect(mapStateToProps)(SettingsComponent));
