21bfe7153998e2840fb9bf6e290038eddc7fb12b
GaspRulez <gasptube@gmail.com>
2020-05-17 20:26:22 +0200

    bozza form settings page

--- front-end/src/components/settingsComponent/index.js
+++ front-end/src/components/settingsComponent/index.js
@@ -3,8 +3,11 @@ import React, {Component, Fragment} from "react";
 import HeaderComponent from "../headerComponent";
 import {connect} from "react-redux";
 import {withCookies} from "react-cookie";
-import {validateToken} from "../../redux/actions/login";
+import {setPassword, setUsername, validateToken} from "../../redux/actions/login";
 import {API_SERVER_URL} from "../../globalConstants";
+import {Camera} from "react-feather";
+import {Link} from "react-router-dom";
+import {setSignUpConfirmPassword, setSignUpPassword} from "../../redux/actions/signup";
 
 const mapStateToProps = (state) => ({...state.userReducer});
 
@@ -12,6 +15,17 @@ class SettingsComponent extends Component {
 
     constructor(props) {
         super(props);
+
+        this.state = {
+            profilePicHover: false,
+            emailFormIsVisible: false,
+            passwordFormIsVisible: false,
+            newEmail: '',
+            confirmNewEmail: '',
+            oldPassword: '',
+            newPassword: '',
+            confirmNewPassword: ''
+        }
     }
 
     componentDidMount() {
@@ -19,17 +33,156 @@ class SettingsComponent extends Component {
         dispatch(validateToken(cookies, history, false, '/settings'));
     }
 
+    togglePicHover() { this.setState({profilePicHover: !this.state.profilePicHover}); }
+
+    toggleEmailForm() {
+        let {emailFormIsVisible, passwordFormIsVisible} = this.state;
+
+        if (passwordFormIsVisible && !emailFormIsVisible)
+            this.togglePasswordForm()
+
+        this.setState({emailFormIsVisible: !this.state.emailFormIsVisible});
+    }
+
+    togglePasswordForm() {
+        let {emailFormIsVisible, passwordFormIsVisible} = this.state;
+
+        if (emailFormIsVisible && !passwordFormIsVisible)
+            this.toggleEmailForm()
+
+        this.setState({passwordFormIsVisible: !this.state.passwordFormIsVisible});
+    }
+
+    editEmailRequest() {}
+
+    editPasswordRequest() {}
+
     render() {
         let {history, userData} = this.props;
+        let {profilePicHover, emailFormIsVisible, passwordFormIsVisible, newEmail, confirmNewEmail, oldPassword, newPassword, confirmNewPassword} = this.state;
+
+        let newEmailHasError = '';
+        let confirmNewEmailHasError = '';
+        let oldPasswordHasError = '';
+        let newPasswordHasError = '';
+        let confirmNewPasswordHasError = '';
+        let messageEditUserError = '';
 
         return (typeof userData !== "undefined") ?
             (
                 <Fragment>
                     <HeaderComponent history={history} />
                     <section className={"d-flex flex-column align-items-center justify-content-center"}>
-                        <div className={"d-flex flex-column align-items-center settingsContainer"}>
-                            <div className={"profilePic"} style={{backgroundImage: `url("${API_SERVER_URL}/uploads/profilePictures/${userData.viewer.picture}")`}}  />
+                        <div className={"settingsContainer"}>
+                            <div
+                                className={"profilePic"}
+                                style={{backgroundImage: `url("${API_SERVER_URL}/uploads/profilePictures/${userData.viewer.picture}")`}}
+                                onMouseEnter={() => this.togglePicHover()}
+                                onMouseLeave={() => this.togglePicHover()}>
+                                {profilePicHover && <Camera size={30} />}
+                            </div>
+                            <h3 className={"mt-2"}>Andrea Gasparini</h3>
                             <p>Hello from settings component!</p>
+                            <span onClick={() => this.toggleEmailForm()}>Clicca qui per modificare la tua email</span>
+                            <span onClick={() => this.togglePasswordForm()}>Clicca qui per modificare la tua password</span>
+                            {emailFormIsVisible &&
+                                <form onSubmit={(e) => this.editEmailRequest(e)} method={"post"} action={"/"}
+                                      className={"w-50 mt-4"}>
+                                    <div className={"form-group"}>
+                                        <div className={"d-flex flex-column"}>
+                                            <input onChange={(e) => {
+                                            }} value={newEmail}
+                                                   required autoComplete={"off"} type={"email"}
+                                                   className={["mb-2 form-control", newEmailHasError ? "is-invalid" : ""].join(" ")}
+                                                   aria-describedby={"emailHelp"} placeholder={"Indirizzo email"}/>
+
+                                            <input onChange={(e) => {
+                                            }} value={confirmNewEmail}
+                                                   required autoComplete={"off"} type={"email"}
+                                                   className={["form-control", confirmNewEmailHasError ? "is-invalid" : ""].join(" ")}
+                                                   aria-describedby={"emailHelp"} placeholder={"Conferma indirizzo email"}/>
+                                        </div>
+                                        {!(newEmailHasError || confirmNewEmailHasError) &&
+                                        <small id={"emailHelp"} className={"form-text text-muted"}>
+                                            Inserisci il nuovo indirizzo email da associare al tuo account.
+                                        </small>
+                                        }
+
+                                        {!(newEmailHasError || confirmNewEmailHasError) &&
+                                        <div className={"invalid-feedback"}>
+                                            {messageEditUserError}
+                                        </div>
+                                        }
+                                    </div>
+
+                                    <div className={"d-flex mt-3 justify-content-end"}>
+                                        <div>
+                                            <button disabled={newEmail.length <= 0 || confirmNewEmail.length <= 0}
+                                                    type={"submit"}
+                                                    className={"btn btn-primary sapienzaButton"}>Conferma
+                                            </button>
+                                        </div>
+                                    </div>
+                                </form>
+                            }
+
+                            {passwordFormIsVisible &&
+                                <form onSubmit={(e) => this.editPasswordRequest()} method={"post"} action={"/"} className={"w-50 mt-4"}>
+                                    <div className={"form-group"}>
+                                        <input onChange={(e) => {}} value={oldPassword}
+                                               required autoComplete={"off"} type={"password"}
+                                               className={["form-control", oldPasswordHasError ? "is-invalid" : ""].join(" ")}
+                                               aria-describedby={"passwordHelp"} placeholder={"Password"}/>
+                                        {!oldPasswordHasError &&
+                                        <small id={"passwordHelp"} className={"form-text text-muted"}>
+                                            Inserisci la password associata al tuo account.
+                                        </small>
+                                        }
+
+                                        {oldPasswordHasError &&
+                                        <div className={"invalid-feedback"}>
+                                            {messageEditUserError}
+                                        </div>
+                                        }
+                                    </div>
+
+                                    <div className={"form-group"}>
+                                        <div className={"d-flex flex-column"}>
+                                            <input onChange={(e) => {}}
+                                                   value={newPassword}
+                                                   required autoComplete={"off"} type={"password"}
+                                                   className={["mb-2 form-control mr-4", newPasswordHasError ? "is-invalid" : ""].join(" ")}
+                                                   aria-describedby={"passwordHelp"} placeholder={"Nuova password"}/>
+
+                                            <input onChange={(e) => {}}
+                                                   value={confirmNewPassword}
+                                                   required autoComplete={"off"} type={"password"}
+                                                   className={["form-control", confirmNewPasswordHasError ? "is-invalid" : ""].join(" ")}
+                                                   aria-describedby={"confirmPasswordHelp"} placeholder={"Conferma nuova password"}/>
+                                        </div>
+                                        {!(newPasswordHasError || confirmNewPasswordHasError) &&
+                                        <small id={"passwordHelp"} className={"form-text text-muted"}>
+                                            Inserisci una nuova password di almeno 8 caratteri per il tuo account.
+                                        </small>
+                                        }
+
+                                        {!(newPasswordHasError || confirmNewPasswordHasError) &&
+                                        <div className={"invalid-feedback"} style={{display: "block"}}>
+                                            {messageEditUserError}
+                                        </div>
+                                        }
+                                    </div>
+
+                                    <div className={"d-flex mt-3 justify-content-end"}>
+                                        <div>
+                                            <button disabled={oldPassword.length <= 0 || newPassword.length <= 0 || confirmNewPassword.length <= 0}
+                                                    type={"submit"}
+                                                    className={"btn btn-primary sapienzaButton"}>Conferma
+                                            </button>
+                                        </div>
+                                    </div>
+                                </form>
+                            }
                         </div>
                     </section>
                 </Fragment>
